565094c169425fcbc38e2a91347cad574e48287b
leandromartinez236 <martinezleandro2004@gmail.com>
2022-03-10 15:06:07 -0300

    completado

--- bash.js
+++ bash.js
@@ -1,36 +1,27 @@
 // bash.js
-const commands = require('./commands/index.js');
+const commands = require("./commands/index.js");
 
-const cmd = 'pwd';
-commands[cmd](); // la función dentro de la propiedad pwd
-
-// Output un prompt
-process.stdout.write('prompt > ');
+process.stdout.write("prompt > ");
 // El evento stdin 'data' se dispara cuando el user escribe una línea
-process.stdin.on('data', (data)=> {
-  const cmd = data.toString().trim(); // remueve la nueva línea
-  if(cmd === 'date') {
-    commands[cmd]();
+process.stdin.on("data", (data) => {
+  const command = data.toString().split(" ")[0];
+  const cmd =
+    command === "echo"
+      ? "echo"
+      : command === "curl"
+      ? "curl"
+      : data.toString().trim();
+  const param = data.toString().slice(5);
+  switch (cmd) {
+    case "date":
+    case "pwd":
+    case "ls":
+    case "echo":
+    case "curl":
+      commands[cmd](param);
+      break;
+    default:
+      process.stdout.write("\nprompt > ");
+      break;
   }
-  if(cmd === 'pwd') {
-    commands[cmd]();
-  }
-  if(cmd === 'ls'){
-    commands[cmd]();
-  }
-  
-  process.stdout.write('\nprompt > ');
 });
-// Output un prompt
-// process.stdout.write('prompt > ');
-// // El evento stdin 'data' se dispara cuando el user escribe una línea
-// process.stdin.on('data', function (data) {
-//   const cmd = data.toString().trim(); // remueve la nueva línea
-//   process.stdout.write('You typed: ' + cmd);
-//   process.stdout.write('\nprompt > ');
-// });
-
-// const commands = require('./commands');
-
-
-// console.log(Object.keys(process));
--- commands/index.js
+++ commands/index.js
@@ -1,23 +1,45 @@
 const fs = require('fs')
-const pwd = (cmd) => {
-  process.stdout.write(process.cwd());
+const request = require('request')
+
+const done=(output)=>{
+  process.stdout.write(output)
+  process.stdout.write("prompt > ")
+}
+
+const pwd = (file) => {
+  const output = process.cwd();
+  done(output)
 };
-const date = (cmd) => {
-  process.stdout.write(Date());
+const date = (file) => {
+  const output = Date();
+  done(output)
 };
-const ls = (cmd) => {
-  fs.readdir(".", (err, files)=> {
-    if (err) throw err;
-    files.forEach((file)=> {
-      process.stdout.write(file.toString() + "\n");
+const ls = (file) => {
+  let output= ''
+  fs.readdir('.', function (err, files) {
+    if(err) throw err
+    files.forEach(function (file) {
+      output += file.toString() + '\n';
     });
-    process.stdout.write("prompt > ");
-  });
+      done(output);
+    });
 };
 
- 
+const echo=(file)=>{
+  const output = file
+  done(output)
+}
+const curl=(file)=>{
+  const output = {}
+ request(file,(err,res,body)=>{
+  done(body)
+ })
+
+}
 module.exports = {
   pwd,
   date,
   ls,
+  echo,
+  curl,
 };
